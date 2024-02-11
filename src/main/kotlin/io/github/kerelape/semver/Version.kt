7fa84f35f23a22bbc94849fa2fe1d84c83688a1b
kerelape <kerelape@gmail.com>
2024-02-11 06:37:49 +0500

    Ignore 'v' prefix

--- src/main/kotlin/io/github/kerelape/semver/Version.kt
+++ src/main/kotlin/io/github/kerelape/semver/Version.kt
@@ -18,7 +18,7 @@ import org.cactoos.scalar.Flattened as FlattenedScalar
 @Suppress("ktlint:standard:max-line-length")
 private val pattern =
     Pattern.compile(
-        "^(?<major>0|[1-9]\\d*)\\.(?<minor>0|[1-9]\\d*)\\.(?<patch>0|[1-9]\\d*)(?:-(?<prerelease>(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+(?<build>[0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?\$",
+        "^v?(?<major>0|[1-9]\\d*)\\.(?<minor>0|[1-9]\\d*)\\.(?<patch>0|[1-9]\\d*)(?:-(?<prerelease>(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+(?<build>[0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?\$",
     )
 
 /**
